c4f5efb30fe5bf5309630df64db779610be5ab78
HoldRain47 <holdrain47@gmail.com>
2025-09-26 17:38:10 +0900

    ADD UPDATE tailwind설치및 파일정리

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,35 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* Tailwind CSS 적용 */}
+      {/* 텍스트 크기 1.875rem, 굵기 두껍게, 색상 빨간색*/}
+      <h1 className="text-3xl font-bold text-red-500">Hello World</h1>
     </>
-  )
+  );
 }
-
-export default App
