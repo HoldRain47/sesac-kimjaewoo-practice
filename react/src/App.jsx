fea05f18a8289720a7377bb9f3e28e377bf5c398
HoldRain47 <holdrain47@gmail.com>
2025-10-02 10:09:26 +0900

    ADD PostLimitSkip 실습

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 //import RecipeContainer from "./components/Recipe/RecipeContainer";
-import ProductContainer from "./components/ProductSort/ProductContainer";
+// import ProductContainer from "./components/ProductSort/ProductContainer";
+import PostContainer from "./components/PostLimitSkip/Container/";
 
 export default function App() {
   return (
     <div>
       {/* <RecipeContainer></RecipeContainer> */}
-      <ProductContainer></ProductContainer>
+      {/* <ProductContainer></ProductContainer> */}
+      <Container></Container>
     </div>
   );
 }
